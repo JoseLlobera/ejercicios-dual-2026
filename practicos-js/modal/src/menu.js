f75ddc9168eae68d1f7ee8fd662685511d24b2c1
JoseLlobera <190204343+JoseLlobera@users.noreply.github.com>
2026-09-13 15:15:33 +0200

    ejercicio-completado-modal

--- practicos-js/modal/src/menu.js
+++ practicos-js/modal/src/menu.js
@@ -1,3 +1,17 @@
 export default (() => {
+    const openButton = document.querySelector(".open")
+    const modal = document.querySelector(".window")
+    const closeButton = document.querySelector(".close")
+
+    function openModal() {
+        modal.hidden = false;
+    }
+
+    function closeModal() {
+        modal.hidden = true;
+    }
+
+    openButton.addEventListener("click", openModal);
+    closeButton.addEventListener("click", closeModal);
 
 })()
